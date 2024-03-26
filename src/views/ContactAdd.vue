c1735d127eda976ec2c5f348ebe43ee9576949fb
caub2014643 <caub2014643@student.ctu.edu.vn>
2024-03-27 03:17:05 +0700

    Tao trang them moi mot lien he

---
+++ src/views/ContactAdd.vue
@@ -0,0 +1,44 @@
+<template>
+    <div v-if="contact" class="page">
+        <h4>Thêm Liên hệ</h4>
+        <ContactForm
+            :contact="contact"
+            @submit:contact="addContact"
+            
+        />
+        <p>{{ message }}</p>
+    </div>
+</template>
+
+<script>
+import ContactForm from "@/components/ContactForm.vue";
+import ContactService from "@/services/contact.service";
+
+export default {
+    components: {
+        ContactForm,
+    },
+    data() {
+        return {
+            contact: {},
+            message: "",
+        };
+    },
+    methods: {
+        
+        async addContact(data) {
+            try {
+                await ContactService.create(data);
+                this.message = "Liên hệ mới được tạo thành công.";
+            } catch (error) {
+                console.log(error);
+            }
+        },
+
+    },
+    /*created() {
+        this.getContact(this.id);
+        this.message = "";
+    },*/
+};
+</script>
